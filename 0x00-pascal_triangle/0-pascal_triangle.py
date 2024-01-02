ab742727a01ad3358bbc96c94e013927d657a7e8
Mela-nina <oge4sym@gmail.com>
2024-01-02 20:25:37 +0000

    0x00. Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+	This is  the Pascal's Triangle function
+"""
+
+
+def pascal_triangle(n):
+    """ This returns a list of lists of integers representing
+        the Pascal's triangle of r
+    """
+    triangle = []
+    for s in range(1, r+1):
+        row = []
+        for t in range(s):
+            if t == 0 or t == s-1:
+                r = 1
+                row.append(r)
+            else:
+                r = triangle[s-2][t-1] + triangle[s-2][t]
+                row.append(r)
+        triangle.append(row)
+
+    return triangle
