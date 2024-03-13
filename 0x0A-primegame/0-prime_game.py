851fc142306fb5389897e4f562b85d9dbbbbe017
Mela-nina <oge4sym@gmail.com>
2024-03-13 19:27:42 +0000

    0x0A. Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,42 +1,42 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 """
-This is the Prime Game
+Prime Game
 """
 
 
 def findMultiples(num, targets):
     """
-    This finds multiples of a given number within a list
+    Finds multiples of a given number within a list
     """
-    for a in targets:
-        if a % num == 0:
-            targets.remove(a)
+    for i in targets:
+        if i % num == 0:
+            targets.remove(i)
     return targets
 
 
-def isPrime(a):
+def isPrime(i):
     """
-    This checks if a number is prime
+    Check if a number is prime.
     """
-    if a == 1:
+    if i == 1:
         return False
-    for b in range(2, a):
-        if a % b == 0:
+    for j in range(2, i):
+        if i % j == 0:
             return False
     return True
 
 
 def findPrimes(n):
     """
-    This dispatches a given set into prime numbers and non-prime numbers
+    Dispatch a given set into prime numbers and non-prime numbers.
     """
     counter = 0
     target = list(n)
-    for a in range(1, len(target) + 1):
-        if isPrime(a):
+    for i in range(1, len(target) + 1):
+        if isPrime(i):
             counter += 1
-            target.remove(a)
-            target = findMultiples(a, target)
+            target.remove(i)
+            target = findMultiples(i, target)
         else:
             pass
     return counter
@@ -44,24 +44,24 @@ def findPrimes(n):
 
 def isWinner(x, nums):
     """
-    Maria and Ben are playing a game.Given a set of consecutive
-    integers starting from 1 up to and including n, they take 
+    Maria and Ben are playing a game Given a set of consecutive
+    integers starting from 1 up to and including n, they take
     turns choosing a prime number from the set and removing that
-    number and its multiples from the set
-    The player that cannot make a move loses the game
+    number and its multiples from the set The player that cannot
+    make a move loses the game
 
-    They play x rounds of the game, where n may be different for each round
-    Assuming Maria always goes first and both players play optimally
-    determine who the winner of each game is
+    They play x rounds of the game, where n may be different for
+    each round Assuming Maria always goes first and both players
+    play optimally determine who the winner of each game is
     """
     players = {'Maria': 0, 'Ben': 0}
     cluster = set()
     for elem in range(x):
         nums.sort()
         num = nums[elem]
-        for a in range(1, num + 1):
-            cluster.add(a)
-            if a == num + 1:
+        for i in range(1, num + 1):
+            cluster.add(i)
+            if i == num + 1:
                 break
         temp = findPrimes(cluster)
 
